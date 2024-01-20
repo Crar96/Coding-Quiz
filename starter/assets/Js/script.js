bccb1c40b4a4d20384b83e49f520b4bfd48aef38
C Grogan <conorgrogan23@gmail.com>
2024-01-20 21:23:14 +0000

    added questions array

---
+++ starter/assets/Js/script.js
@@ -0,0 +1,6 @@
+const questions = [
+  { question: "Which event is specific to the keyboard?", anwsers: ["onkeydown", "onkeypush", "oninputreceived", "onkeybinput"], correct: 0},
+  { question: "how do you call a function?", anwsers: ["result myFunction()", "call myFunction()", "result = myFunction()", "result = myFunction()"], correct: 2},
+  { question: "What is the correct place to insert JavaScript?", anwsers: ["The head section","The body section","both the head and body sections are correct"], correct: 2},
+  { question: "How does a for loop start?", anwsers: ["for (i <= 5; i++)","for i = 1 to 5","for (i = 0; i < = 5), for (i = 0; i < = 5; i++)"], correct: 3},
+]
