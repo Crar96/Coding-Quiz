f259d1ef1776038b18538c5b6584869732271857
C Grogan <conorgrogan23@gmail.com>
2024-01-20 21:46:37 +0000

    started creating variable declerations

--- starter/assets/Js/script.js
+++ starter/assets/Js/script.js
@@ -1,6 +1,15 @@
+
+// Array containing questions
 const questions = [
   { question: "Which event is specific to the keyboard?", anwsers: ["onkeydown", "onkeypush", "oninputreceived", "onkeybinput"], correct: 0},
   { question: "how do you call a function?", anwsers: ["result myFunction()", "call myFunction()", "result = myFunction()", "result = myFunction()"], correct: 2},
   { question: "What is the correct place to insert JavaScript?", anwsers: ["The head section","The body section","both the head and body sections are correct"], correct: 2},
-  { question: "How does a for loop start?", anwsers: ["for (i <= 5; i++)","for i = 1 to 5","for (i = 0; i < = 5), for (i = 0; i < = 5; i++)"], correct: 3},
-]
+  { question: "How does a for loop start?", anwsers: ["for (i <= 5; i++)","for i = 1 to 5","for (i = 0; i < = 5), for (i = 0; i < = 5; i++)"], correct: 2},
+];
+
+// Dom elements
+
+const startButton = document.getElementById("start");
+const questionTitle = document.getElementById("question-title");
+const startScreen = document.getElementById("start-screen");
+const 
