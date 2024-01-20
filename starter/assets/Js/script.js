c40ef0b532dfedfdfb73ded85c0d046603be05f2
C Grogan <conorgrogan23@gmail.com>
2024-01-20 23:38:38 +0000

    added variables to evaluate the current state of the quiz and track the score, current question and time

--- starter/assets/Js/script.js
+++ starter/assets/Js/script.js
@@ -1,4 +1,5 @@
 //DomContentLoaded function makes sure that the JavaScript executes only after the document is loaded.
+
 // Variable declerations allows interaction with HTML elements using JavaScript (HTML elements are all assigned an ID in this case)
 document.addEventListener("DOMContentLoaded", function() {
   const startButton = document.getElementById("start");
@@ -11,14 +12,23 @@ document.addEventListener("DOMContentLoaded", function() {
   const endScreen = document.getElementById("end-screen");
   const finalScore = document.getElementById("final-score");
   const feedbackBox = document.getElementById("feedback");
-}
 
+// variables storing the current state of the quiz including the score and timer.
+  let currentQuestion = 0;
+  let score = 0;
+  let timer;
 
-// Array containing questions
+// Array containing questions and anwsers
 const questions = [
   { question: "Which event is specific to the keyboard?", anwsers: ["onkeydown", "onkeypush", "oninputreceived", "onkeybinput"], correct: 0},
   { question: "how do you call a function?", anwsers: ["result myFunction()", "call myFunction()", "result = myFunction()", "result = myFunction()"], correct: 2},
   { question: "What is the correct place to insert JavaScript?", anwsers: ["The head section","The body section","both the head and body sections are correct"], correct: 2},
   { question: "How does a for loop start?", anwsers: ["for (i <= 5; i++)","for i = 1 to 5","for (i = 0; i < = 5), for (i = 0; i < = 5; i++)"], correct: 2},
 ];
+
+//event listners that initialize the quiz on click of the start button and save the score when the user click submit for an anwser.
+startButton.addEventListener("click", initializeQuiz);
+submitButton.addEventListener("click", saveScore); 
+
+
 
